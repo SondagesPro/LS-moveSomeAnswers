67b7ca7204135f7e99810a3e9c99502f6eb9a55c
Denis Chenu <denis@sondages.pro>
2017-02-20 18:29:36 +0100

    [fix] 2.63.0 Limesurvey broke API

--- moveSomeAnswers.php
+++ moveSomeAnswers.php
@@ -17,7 +17,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-class moveSomeAnswers extends PluginBase
+class moveSomeAnswers extends \ls\pluginmanager\PluginBase
 {
 
     protected $storage = 'DbStorage';
